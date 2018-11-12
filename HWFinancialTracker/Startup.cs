ca9b88e95d88352eed13a97a247a8faeec4a9a21
Hanif Warren <Hanif.warren@gmail.com>
2018-11-12 07:29:14 -0500

    Add project files.

---
+++ HWFinancialTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HWFinancialTracker.Startup))]
+namespace HWFinancialTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
