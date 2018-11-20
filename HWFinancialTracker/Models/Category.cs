7f077492d7945aa8e7c0cf203f9782cdfca0b7b1
Hanif Warren <Hanif.warren@gmail.com>
2018-11-19 21:25:24 -0500

    Made notes for Category class (even what the class is for, and why HashSet is used)

--- HWFinancialTracker/Models/Category.cs
+++ HWFinancialTracker/Models/Category.cs
@@ -5,18 +5,24 @@ using System.Web;
 
 namespace HWFinancialTracker.Models
 {
+    //Per SRS, Transactions and Expenses are categorized (i.e. what did you spend $5000 on???? a new hat?!)
     public class Category
     {
+        //this ensures that every category object comes with "space" for the households that are on it.  It will "roll up"
         public Category()
         {
+            //"roll up" ticket histories into a collection (doesn't have to be HashSet) and include it the Category object when it gets instantiated.
             this.Households = new HashSet<Household>();
         }
+        //primary key for Categories
         public int Id { get; set; }
+        //name of the Category (e.g.
         public string Name { get; set; }
 
-        //navigation property
+        //foreign key for Households
         public int HouseholdId { get; set; }
 
+        //navigation property to connect the Household to Categories
         public virtual ICollection<Household> Households { get; set; }
 
 
