8c32023b50ef45aae02c2827989a662e55fba48b
Hanif Warren <Hanif.warren@gmail.com>
2018-11-19 21:08:45 -0500

    Commented Budget.cs and BudgetItem.cs for clarity (and being able to recall design/code decisions in the future)

--- HWFinancialTracker/Models/Budget.cs
+++ HWFinancialTracker/Models/Budget.cs
@@ -7,12 +7,14 @@ namespace HWFinancialTracker.Models
 {
     public class Budget
     {
+        //primary key of the my budget
         public  int Id { get; set; }
+        //Identifiable name of that budget e.g. "Travel budget"
         public int Name { get; set; }
-
+        //Primary key of the household to which this budget belongs. It goes hand in hand with the navigation property
         public int HouseholdId { get; set; }
 
-
+        //navigation property of my household, it creates the relationship using entity framework and it allows e.g. me to click "back to household" and it will show the household (and all budgets the household belongs to)
         public virtual Household Household { get; set; }
     }
 }
--- HWFinancialTracker/Models/BudgetItem.cs
+++ HWFinancialTracker/Models/BudgetItem.cs
@@ -7,13 +7,19 @@ namespace HWFinancialTracker.Models
 {
     public class BudgetItem
     {
+        //primary key of this budget item (e.g. "gas", which would go in the transportation budget)
         public int Id { get; set; }
+        //the amount that the budget costs (e.g. gas cost $150 last month)
         public int Amount { get; set; }
-
+        // primary key of the category this budget belongs to. can use to list the Category name
         public int CategoryId { get; set; }
+        //primary key of the category this budget belongs to. Can use to list the
         public int BudgetId { get; set; }
 
+        //these navigation properties create the relationship (1 to many, 1 Budget has many Budget Items)
         public virtual Budget Budgets { get; set; }
+        //these navigation properties create the relationship (1 to many, 1 Category has many Budget Items)
+
         public virtual Category Categories { get; set; }
     }
 }
