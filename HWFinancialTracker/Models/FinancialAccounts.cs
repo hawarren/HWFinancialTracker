ec9394c218982761e97859538e056f0975d75cce
Hanif Warren <Hanif.warren@gmail.com>
2018-11-19 21:34:49 -0500

    Annotated the Financial Accounts Class.

--- HWFinancialTracker/Models/FinancialAccounts.cs
+++ HWFinancialTracker/Models/FinancialAccounts.cs
@@ -7,19 +7,28 @@ namespace HWFinancialTracker.Models
 {
     public class FinancialAccounts
     {
+        //wraps up all the transactions on this account into a nice collection (for iterating over, adding them up, and other manipulations)
         public FinancialAccounts()
         {
             this.Transactions = new HashSet<Transaction>();
         }
+        //primary key of the financial account
         public int Id { get; set; }
+        //Name of the account (e.g. bank Account?) or billing account (PSEG?)
         public string Name { get; set; }
+        //how much is remaining in the account
         public int Balance { get; set; }
+        //How much is remaining after balance is reconciled (e.g. when balancing checkbook, accounting for pending transactions)
         public int ReconciledBalance { get; set; }
 
+        //foreign key for Household
         public int HouseholdId { get; set; }
+        //foreign key for Transactions
         public int TransactionId { get; set; }
 
+        //navigation property for Transactions (each account can/will have many transactions
         public virtual ICollection<Transaction> Transactions { get; set; }
+        //navigation property for the Household (each Transaction belongs to a single Household)
         public virtual Household Household { get; set; }
 
     }
