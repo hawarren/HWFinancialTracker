ea200a944420f90ccac5b8a760f224520468b5f3
Hanif Warren <Hanif.warren@gmail.com>
2018-11-19 21:28:11 -0500

    Annotated EmailModel class with notes on each property.

--- HWFinancialTracker/Models/EmailModel.cs
+++ HWFinancialTracker/Models/EmailModel.cs
@@ -6,17 +6,22 @@ using System.Web;
 
 namespace HWFinancialTracker.Models
 {
+    //This is for generating Email notifications with a specific message. Such as "you spent $100 today on clothing")
     public class EmailModel
     {
+        //Who the sender is (admin? the head of household?)
         [Required, Display(Name = "Name")]
         public string FromName { get; set; }
 
+        //Email address of the sender
         [Required, Display(Name = "Email"), EmailAddress]
         public string FromEmail { get; set; }
 
+        //Subject line of the email message
         [Required]
         public string Subject { get; set; }
 
+        //content of the email message
         [Required]
         public string Body { get; set; }
 
