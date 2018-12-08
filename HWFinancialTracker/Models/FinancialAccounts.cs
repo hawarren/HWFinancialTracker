716d13d0975b96ea6c44c38fe02c7acd1515a433
Hanif Warren <Hanif.warren@gmail.com>
2018-12-08 15:14:38 -0500

    update to bootstrap 4  broke default vs2015 theme. Fixed using bootstrap 3to4 converter at http://upgrade-bootstrap.bootply.com/ Added new layout page for bootstrap 4 and changed viewstart to use that page, in case I want to revert to bootstrap 3

--- HWFinancialTracker/Models/FinancialAccounts.cs
+++ HWFinancialTracker/Models/FinancialAccounts.cs
@@ -7,34 +7,37 @@ namespace HWFinancialTracker.Models
 {
     public class FinancialAccounts
     {
-        //wraps up all the transactions on this account into a nice collection (for iterating over, adding them up, and other manipulations)
+        // wraps up all the transactions on this account into a nice collection (for iterating over, adding them up, and other manipulations)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FinancialAccounts"/> class.
+        /// </summary>
         public FinancialAccounts()
         {
             this.Transactions = new HashSet<Transaction>();
         }
-        //primary key of the financial account
+        // primary key of the financial account
         public int Id { get; set; }
 
-        //Name of the account (e.g. bank Account?) or billing account (PSEG?)
+        // Name of the account (e.g. bank Account?) or billing account (PSEG?)
         public string Name { get; set; }
 
-        //how much is remaining in the account before reconciliation?
+        // how much is remaining in the account before reconciliation?
         public int Balance { get; set; }
 
-        //How much is remaining after balance is reconciled (e.g. when balancing checkbook, accounting for pending transactions)
+        // How much is remaining after balance is reconciled (e.g. when balancing checkbook, accounting for pending transactions)
         public int ReconciledBalance { get; set; }
 
 
-        //foreign key for Household
+        // foreign key for Household
         public int HouseholdId { get; set; }
 
-        //foreign key for Transactions
+        // foreign key for Transactions
         public int TransactionId { get; set; }
 
-        //navigation property for Transactions (each account can/will have many transactions
+        // navigation property for Transactions (each account can/will have many transactions
         public virtual ICollection<Transaction> Transactions { get; set; }
 
-        //navigation property for the Household (each Transaction belongs to a single Household)
+        // navigation property for the Household (each Transaction belongs to a single Household)
         public virtual Household Household { get; set; }
 
     }
